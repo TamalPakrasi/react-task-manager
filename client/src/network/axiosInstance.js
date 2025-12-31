064220132bec00b391aa521ec52026dd20731a31
Tamal Pakrasi <tamalpakrasi11@gmail.com>
2025-12-31 22:30:30 +0530

    Fix deployment bug 6

--- client/src/network/axiosInstance.js
+++ client/src/network/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const publicApi = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -9,7 +9,7 @@ export const publicApi = axios.create({
 });
 
 export const privateApi = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
